dc9a237f9a71c4b20b46229a70911b9cc9c5a9c6
Aniket Bhatia <aniketbhatia9@gmail.com>
2025-09-05 22:06:47 +0530

    Updated NextConfig

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // enables static HTML export
-  distDir: 'out',   // folder where exported static files will go
-};
-
-module.exports = nextConfig;
